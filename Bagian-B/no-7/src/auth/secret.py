20b1a55820363767e6165bb047c9b9a3be96e507
susTuna <hidasetsuna@gmail.com>
2025-08-06 20:13:06 +0700

    feat: id generation

--- Bagian-B/no-7/src/auth/secret.py
+++ Bagian-B/no-7/src/auth/secret.py
@@ -17,6 +17,33 @@ def concat_secret(name: str, email: str, uri: Optional[str] = None) -> str:
         return f"{name[:1]}:{email[2:]}:{uri[:5]}:{email[:2]}:{name[1:]}"
     return f"{name[:1]}:{email[2:]}:{email[:2]}:{name[1:]}"
 
+def generate_id_key(msg: str, key: str) -> str:
+    key = list(key)
+    if len(msg) == len(key): return key
+    for i in range(len(msg) - len(key)):
+        key.append(key[i % len(key)])
+    return ''.join(key)
+
+def generate_id_p1(msg: str, key: str) -> str:
+    id = []
+    key = generate_id_key(msg, key)
+    for i in range(len(msg)):
+        char = msg[i]
+        if char.isupper():
+            id.append(chr((ord(char) + ord(key[i])) - 2 * ord('A')) % 26 + ord('A'))
+        elif char.islower():
+            id.append(chr((ord(char) + ord(key[i])) - 2 * ord('a')) % 26 + ord('a'))
+        elif char.isdigit():
+            id.append(chr((ord(char) + ord(key[i])) - 2 * ord('0')) % 10 + ord('0'))
+        else:
+            id.append(char)
+    return ''.join(id)
+
+def generate_id(client: ClientRegister) -> str:
+    msg = concat_secret(client.name, client.email, client.uri)
+    key = base64.b64encode(msg[:9].encode()).decode()
+    return generate_id_p1(msg, key)
+
 def fun_roulette() -> int:
     return randint(1,6)
 
